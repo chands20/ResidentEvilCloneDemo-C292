d92f4f8b0a47429fa376e9050090f7bf67f611b8
chands20 <cahands20@gmail.com>
2024-10-01 17:49:28 -0400

    add animation

---
+++ Resident Evil Clone/Assets/Scripts/Door.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    private Animator animator;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DoorMoved()
+    {
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            animator.SetTrigger("ToggleDoor");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            animator.SetTrigger("ToggleDoor");
+        }
+    }
+
+}
